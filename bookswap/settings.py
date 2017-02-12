b0fb0cde997f429b536710f37e83fe7d61589bbf
Ufuk Doğan <ufukdogan92@gmail.com>
2017-02-12 22:24:14 +0300

    Allow all host

--- bookswap/settings.py
+++ bookswap/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'whom)!5jf1iy9n=h5t4zxv8gr41g)#3@hlx3)kcpexe9ic(#77'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*',]
 
