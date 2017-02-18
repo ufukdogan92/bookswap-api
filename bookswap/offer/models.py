f1cf52f35dda2398cca1e239a38c78b353eced13
Ufuk Doğan <ufukdogan92@gmail.com>
2017-02-18 03:18:28 +0300

    Add long and lat on offer

--- bookswap/offer/models.py
+++ bookswap/offer/models.py
@@ -11,6 +11,8 @@ class Offer(models.Model):
     author = models.ForeignKey(Author,unique=False)
     created_date = models.DateTimeField(auto_now=False, auto_now_add=True)
     title = models.CharField(max_length=50)
+    Latitude  = models.DecimalField(max_digits=20,decimal_places=10)
+    Longitude = models.DecimalField(max_digits=20,decimal_places=10)
     descr = models.TextField(max_length=220)
     image = models.ImageField(upload_to="books")
     is_active = models.BooleanField(default=True)
