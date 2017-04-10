a5c2307b3c8a80de179dc0b082110515d354074f
ufukdogan92 <ufukdogan92@gmail.com>
2017-04-10 14:49:48 +0300

    Delete id on user_profile

--- bookswap/user_profile/serializers.py
+++ bookswap/user_profile/serializers.py
@@ -5,7 +5,7 @@ from rest_framework import serializers
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile 
-        fields = ('id','user','gender','birthdate','phone','image')
+        fields = ('user','gender','birthdate','phone','image')
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
